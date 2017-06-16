6779bf0bc3bdb98dadef7bddeb61ce2abfa0fc17
Igwe Magnus Nnamdi <igwemagnusnnamdi@gmail.com>
2017-06-16 20:36:51 +0900

    PHP and postgreSQL Project

---
+++ PHPnPostgreSQL9_EssT/ExerciseFiles/CRUD/assets/footer.php
@@ -0,0 +1,13 @@
+
+<div class="footer">
+<p class="copyright">
+    <?php echo $CRUD['DBVERSION'] ?>
+    &middot;
+    CRUD Version <?php echo VERSION ?>
+    <br />
+    Copyright &copy; 2009-2017
+    <a href="http://bhg.bw.org/" target="_blank">Igwe Magnus Nnamdi</a\>
+</p>
+
+</body>
+</heml>
---
+++ PHPnPostgreSQL9_EssT/ExerciseFiles/Testimonials/assets/footer.php
@@ -0,0 +1,16 @@
+<p class="clear" />
+</div> <!-- content -->
+
+<div id="copyright">
+    <p> 
+    <a href="<?php echo($G['SELF']) ?>">Testimonials Database</a> &middot;
+    <?php echo($G['VERSION']) ?> &middot;
+    by <a href="http://bw.org/">Igwe Magnus Nnamdi</a> &middot;
+    Copyright &copy; 2004-2017 <a href="http://bhg.bw.org/">Igwe Magnus Nnamdi</a>
+    </p>
+</div>
+
+</p>
+
+</body>
+</html>
